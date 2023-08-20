41ab4d530cfbdf8818d96b4bef8c2e8d48ef66ff
Obinna Akuma <akumavincent140@gmail.com>
2023-08-21 00:32:59 +0100

    Second Commit

--- app/src/main/java/o/akuma/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/o/akuma/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,20 @@ package o.akuma.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import o.akuma.a7minutesworkout.databinding.ActivityExerciseBinding
 
+@Suppress("DEPRECATION")
 class ExerciseActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityExerciseBinding
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+    private var timePassed = 10
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -25,4 +34,22 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
     }
+
+    private fun restProgressBar(){
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = timePassed - restProgress
+                binding.tvTimer.text = (timePassed - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Here now we will start the exercise.",Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }.start()
+    }
+
 }
